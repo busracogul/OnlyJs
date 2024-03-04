80e4d76166a15c827f3871f90e44480f0318b3f1
Büşra Çoğul <cogulbusra@gmail.com>
2024-03-04 12:41:22 +0300

    refactor: stopwatch edited

--- JS-9793P/assets/script.js
+++ JS-9793P/assets/script.js
@@ -11,51 +11,50 @@ function resetNumber() {
 
 //kronomotre
 let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-let timeRef = document.querySelector(".timer-display");
-let int = null;
+const timeRef = document.getElementById("timer-display");
+let interval = null;
+const delay = 100;
 
 document.getElementById("start-timer").addEventListener("click", () => {
-    if (int !== null) {
-        clearInterval(int);
+    if (interval !== null) {
+        clearInterval(interval);
     }
-    int = setInterval(displayTimer, 10);
+    interval = setInterval(displayTimer, delay);
 });
 
 document.getElementById("pause-timer").addEventListener("click", () => {
-    clearInterval(int);
+    clearInterval(interval);
 });
 
 document.getElementById("reset-timer").addEventListener("click", () => {
-    clearInterval(int);
+    clearInterval(interval);
     [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
     timeRef.innerHTML = "00 : 00 : 00 : 000 ";
 });
 
 function displayTimer() {
-    milliseconds += 10;
-    if (milliseconds == 1000) {
+    milliseconds += delay;
+    if (milliseconds === 1000) {
         milliseconds = 0;
         seconds++;
-        if (seconds == 60) {
-            seconds = 0;
-            minutes++;
-            if (minutes == 60) {
-                minutes = 0;
-                hours++;
-            }
-        }
+    }
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+    if (minutes === 60) {
+        minutes = 0;
+        hours++;
     }
 
-    let h = hours < 10 ? "0" + hours : hours;
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
     let ms =
-        milliseconds < 10
-            ? "00" + milliseconds
-            : milliseconds < 100
-                ? "0" + milliseconds
-                : milliseconds;
+        milliseconds < 100
+            ? "0" + milliseconds
+            : milliseconds;
+
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let h = hours < 10 ? "0" + hours : hours;
 
     timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
-
 }
