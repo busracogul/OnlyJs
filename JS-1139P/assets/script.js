6cc7a7cd7966eae05bc24bdc37f28ee4e5938c25
Büşra Çoğul <cogulbusra@gmail.com>
2024-03-07 15:01:54 +0300

    refactor: functions edited

--- JS-1139P/assets/script.js
+++ JS-1139P/assets/script.js
@@ -1,49 +1,81 @@
-function changeName(team) {
-    let changeTeamName = prompt('Takım ismini giriniz : ');
+let teamName1 = "Takım A";
+let teamName2 = "Takım B";
+let teamScore1 = 0;
+let teamScore2 = 0;
 
-    if (changeTeamName !== null && changeTeamName.trim() !== '') {
-        document.getElementById(`${team}TeamName`).innerText = changeTeamName;
+function changeName(team) {
+    if (typeof team !== "string" || team.trim().length < 1 || team === null || team === undefined) {
+        console.error('hatalı değer girdiniz');
+        return;
     }
-    else {
-        alert('Geçerli bir takım ismi girmelisiniz!')
+    let changeTeamName = prompt('Takım ismini giriniz : ');
+    if (changeTeamName === null || changeTeamName.trim().length < 1 || changeTeamName === undefined) {
+        alert('Geçerli bir takım ismi girmelisiniz!');
+        return;
+    }
+    if (team === 'first') {
+        teamName1 = changeTeamName;
+        document.getElementById('firstTeamName').innerText = teamName1;
+        return;
+    }
+    if (team === 'second') {
+        teamName2 = changeTeamName;
+        document.getElementById('secondTeamName').innerText = teamName2;
+        return;
     }
 }
-
 function incrementScore(team) {
     const scoreId = `${team}Score`;
     const scoreElement = document.getElementById(scoreId);
-    let score = parseInt(scoreElement.textContent);
-    scoreElement.textContent = ++score;
+    let currentScore = parseInt(scoreElement.textContent);
+    scoreElement.textContent = ++currentScore;
 }
-
 function decraseScore(team) {
     const scoreId = `${team}Score`;
     const scoreElement = document.getElementById(scoreId);
-    let score = parseInt(scoreElement.textContent);
+    let currentScore = parseInt(scoreElement.textContent);
 
-    if (score > 0) {
-        scoreElement.textContent = --score;
+    if (currentScore > 0) {
+        scoreElement.textContent = --currentScore;
     }
 }
-
 function changeScore(team) {
-    let currentScore = prompt('Gol sayısını giriniz : ');
-    if (currentScore !== null && currentScore.trim() !== '' && currentScore > 0) {
-        document.getElementById(`${team}Score`).innerText = currentScore;
+    let currentScore = Number(prompt('Gol sayısını giriniz : '));
+    if (typeof currentScore !== 'number' || currentScore === null || currentScore === undefined || currentScore < 1 || isNaN(currentScore)) {
+        alert('Geçerli bir skor girmelisiniz!');
+        return;
     }
-    else {
-        alert('Lütfen geçerli bir skor giriniz: ');
+    if (team === 'first') {
+        teamScore1 = currentScore;
+        document.getElementById('firstScore').innerText = teamScore1;
+        return;
+    }
+    if (team === 'second') {
+        teamScore2 = currentScore;
+        document.getElementById('secondScore').innerText = teamScore2;
+        return;
     }
 }
-
 function allReset(reset) {
-    const number = 0;
-    document.getElementById(`${reset}Score`).innerText = number;
-
+    const resetScore = 0;
     document.getElementById("firstTeamReset").addEventListener("click", function () {
         document.getElementById('firstTeamName').innerText = "Takım A";
+        document.getElementById('firstScore').innerText = resetScore;
     })
     document.getElementById("secondTeamReset").addEventListener("click", function () {
-        document.getElementById('secondTeamName').innerText = "Takım B";
+        document.getElementById('secondTeamName').textContent = "Takım B";
+        document.getElementById('secondScore').innerText = resetScore;
     })
 }
+
+window.onload = () => {
+    const teamNameElement = document.getElementById('firstTeamName');
+    teamNameElement.innerHTML = teamName1;
+    const teamNameElement2 = document.getElementById('secondTeamName');
+    teamNameElement2.innerHTML = teamName2;
+
+    const teamScoreElement = document.getElementById('firstScore');
+    teamScoreElement.innerHTML = teamScore1;
+    const teamScoreElement2 = document.getElementById('secondScore');
+    teamScoreElement2.innerHTML = teamScore2;
+};
