acd0840636c9855326b189feea85bed526eea1cc
Büşra Çoğul <cogulbusra@gmail.com>
2024-04-03 17:10:23 +0300

    refactor: card style changes

--- JS-288P-1/assets/users.js
+++ JS-288P-1/assets/users.js
@@ -9,14 +9,14 @@ async function fetchUsers() {
       userElement.classList.add("col-lg-3", "col-md-6", "my-3");
       userElement.innerHTML = `
             <div class="card h-100" style="width: 18rem;">
-            <div class="card-body">
+            <div class="card-body card-user">
               <h5 class="card-title">${user.name} ${user.username}</h5>
               <p class="card-text"><i class="bi bi-geo-alt-fill"></i> ${user.address.street}, ${user.address.suite}, ${user.address.city}</p>
               <p class="card-text"><i class="bi bi-building-fill"></i> ${user.company.name}</p>
               <p class="card-text"><i class="bi bi-envelope-fill"></i> ${user.email}</p>
               <p class="card-text"><i class="bi bi-telephone-fill"></i> ${user.phone}</p>
               <p class="card-text"><i class="bi bi-globe"></i> ${user.website}</p>
-              <a href="posts.html?userId=${user.id}" class="btn btn-outline-success">Detaylar</a>
+              <a href="posts.html?userId=${user.id}" class="btn btn-outline-info d-flex justify-content-center">Gönderileri Görüntüle</a>
             </div>
           </div>`;
       usersWrapper.appendChild(userElement);
